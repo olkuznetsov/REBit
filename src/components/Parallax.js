a4efac4d8274ae929435e167664bc4117f465df0
Oleksandr <askuznetsov6996@gmail.com>
2025-01-07 21:44:14 +0200

    added carousel pictures

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './Parallax.css'; // Assuming you will create a CSS file for styling
+import React from "react";
+import "./Parallax.css";
 
 const Parallax = () => {
-    return (
-        <div className="parallax" style={{ backgroundImage: `url('/src/assets/images/parallax-image.jpg')` }}>
-            <div className="parallax-content">
-                <h1>Welcome to REBit Store</h1>
-                <p>Discover amazing products</p>
-            </div>
-        </div>
-    );
+  return (
+    <div
+      className='parallax'
+      style={{ backgroundImage: `url('/assets/images/parallax-image.jpg')` }}>
+      <div className='parallax-content'>
+        <h1>Welcome to REBit Store</h1>
+        <p>Discover amazing products</p>
+      </div>
+    </div>
+  );
 };
 
 export default Parallax;
